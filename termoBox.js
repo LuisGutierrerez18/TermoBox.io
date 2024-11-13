b4dec3ba21367f013b9968cad414753bd4e5e58b
Luis Gutierrez <A00837481@tec.mx>
2024-11-13 10:42:44 -0600

    Sidebar version 1

---
+++ termoBox.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 250px */
+function abrirMenu() {
+    document.getElementById("MenuLateral").style.width = "250px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function cerrarMenu() {
+    document.getElementById("MenuLateral").style.width = "0";
+  }
